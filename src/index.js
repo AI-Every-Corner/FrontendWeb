0043511eebcdfee46c53029a6cd5764149030757
Brian <java202406@gmail.com>
2024-10-04 11:28:39 +0800

    feat: header update logout user img

--- src/index.js
+++ src/index.js
@@ -4,16 +4,16 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MainPage from './mainpage'
 import Blank from './blank';
 import Components from './components';
-import Friends from './Friends';
+import Friends from './friends';
 import Groups from './groups';
 import MarcketPlace from './marketplace';
 import Messages from './messages';
 import Modal from './modal';
 import NewsFeed2 from './newsfeed-2';
-import Profile from './Profile';
+import Profile from './profile';
 import About from './About';
 import Photo from './Photo';
-import Settings from './Settings';
+import Settings from './settings';
 import SignIn from './sign-in';
 import SignUp from './sign-up';
 // import Widgets from './widgets';
@@ -25,26 +25,32 @@ import Header from './Header';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <UserProvider>
-  <Router>
-    <Header />
-    <div className="main-content">
-    <Routes>
-    <Route path='/' element={<PrivateRoute element={MainPage} />} />
-      <Route path='/blanck' element={<PrivateRoute element={Blank} />} />
-      <Route path='/components' element={<PrivateRoute element={Components} />} />
-      <Route path='/friends' element={<PrivateRoute element={Friends} />} />
-      <Route path='/groups' element={<PrivateRoute element={Groups} />} />
-      <Route path='/marcketplace' element={<PrivateRoute element={MarcketPlace} />} />
-      <Route path='/messages' element={<PrivateRoute element={Messages} />} />
-      <Route path='/modal' element={<PrivateRoute element={Modal} />} />
-      <Route path='/newsfeed-2' element={<PrivateRoute element={NewsFeed2} />} />
-      <Route path='/profile' element={<PrivateRoute element={Profile} />} />
-      <Route path='/settings' element={<PrivateRoute element={Settings} />} />
-      <Route path='/sign-in' element={<SignIn />} />
-      <Route path='/sign-up' element={<SignUp />} />
-    </Routes>
-    </div>
-  </Router>
+    <Router>
+      <Routes>
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/*' element={
+          <>
+            <Header />
+            <div className="main-content">
+              <Routes>
+                <Route path='/' element={<PrivateRoute element={MainPage} />} />
+                <Route path='/blanck' element={<PrivateRoute element={Blank} />} />
+                <Route path='/components' element={<PrivateRoute element={Components} />} />
+                <Route path='/friends' element={<PrivateRoute element={Friends} />} />
+                <Route path='/groups' element={<PrivateRoute element={Groups} />} />
+                <Route path='/marcketplace' element={<PrivateRoute element={MarcketPlace} />} />
+                <Route path='/messages' element={<PrivateRoute element={Messages} />} />
+                <Route path='/modal' element={<PrivateRoute element={Modal} />} />
+                <Route path='/newsfeed-2' element={<PrivateRoute element={NewsFeed2} />} />
+                <Route path='/profile' element={<PrivateRoute element={Profile} />} />
+                <Route path='/settings' element={<PrivateRoute element={Settings} />} />
+              </Routes>
+            </div>
+          </>
+        } />
+      </Routes>
+    </Router>
   </UserProvider>
 );
 
